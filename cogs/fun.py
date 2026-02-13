b860a44d0a65745ebd51aa7a8a08f94c1404ab49
notquad8 <71942089+notquad8@users.noreply.github.com>
2026-02-13 15:34:13 +0300

    Refine random answers in Fun cog
    Updated responses in randomAnswerBall for clarity.

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 class Fun(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-        self.randomAnswerBall = ['Духи говорят "Да". Число пи...', 'Конечно нет, ты шо?', 'Я глухой, повтори.', 'Э-э-э-э. А? Ч-что ты имеешь виду?', 'Да', 'Я не знаю(тут моя фонтазия кончилась).']
+        self.randomAnswerBall = ['Духи говорят "Да".', 'Духи говорят "Нет"', 'Повтори.', 'Не уверен', 'Да', 'Конечно']
         self.weather_info = 'https://www.google.com/search?sxsrf=ALeKk02RS2OLulk15WTgcF8EPx2Kd7V18w%3A1599998140219&ei=vAheX7XmDM2nrgTEwLugDw&q=%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0+%D0%BC%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&oq=%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0+%D0%BC%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&gs_lcp=CgZwc3ktYWIQAzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQR1CxIFixIGDOImgAcAJ4AIABAIgBAJIBAJgBAKABAaoBB2d3cy13aXrIAQjAAQE&sclient=psy-ab&ved=0ahUKEwi1tKfGiebrAhXNk4sKHUTgDvQQ4dUDCAw&uact=5'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0'}
         self.dollar_rub = 'https://www.google.com/search?sxsrf=ALeKk01faovKHhed2JGtVzwzuHGLeOlNcQ%3A1598179078575&source=hp&ei=BkdCX5vDIMqTlwStjruADA&q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0&oq=rehc&gs_lcp=CgZwc3ktYWIQARgAMgcIIxCxAhAnMggIABAKEAEQQzIICAAQChABEEMyBAgAEAoyBAgAEEMyBAgAEAoyBAgAEAoyBAgAEAoyBAgAEAoyBAgAEAo6BAgjECc6BQgAELEDOggILhCxAxCDAToHCAAQsQMQQzoECC4QQzoKCAAQChABEEMQKjoGCAAQChABOggILhAKEAEQQ1CeBFjmCWC7FmgAcAB4AIABmQGIAbQEkgEDMC40mAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab'
